b057912b0187d6d853f7624e2c594403a2594cd6
Saas112 <151405383+Saas112@users.noreply.github.com>
2023-11-20 16:01:49 -0300

    Add files via upload

---
+++ Dino/cactus.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+
+class Cacto:
+    def __init__(self):
+        self.x = 600
+        self.y = 256
+        self.velocidade = 10
+        self.rect = pygame.Rect(self.x, self.y, 20, 40)
+
+    def movimentacao(self):
+        self.x -= self.velocidade + random.randint(0, 8)
+        if self.x < -15:
+            self.x = random.randint(600, 750)
+            if self.velocidade < 30:
+                self.velocidade += 0.1
+        
+        self.rect = pygame.Rect(self.x, self.y, 20, 40) 
+
+    def desenhar(self, screen):
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, 20, 40))
+
+    def colisao(self, dino):
+        if self.rect.colliderect(dino.rect):
+            print(f"Você perdeu! Sua pontuação foi de: {dino.pixels_count//10} pontos")
+            pygame.quit()
---
+++ Dino/dino.py
@@ -0,0 +1,42 @@
+import player, cactus, chao, pygame, random
+
+# Loop principal
+def main():
+    try:
+        dino1 = player.Dino()
+        cacto1 = cactus.Cacto()
+        chao1 = chao.Chao()
+
+        while True:
+            
+            clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            dino1.movimentacao()
+            cacto1.movimentacao()
+            chao1.movimentacao()
+
+            dino1.desenhar(screen)
+            cacto1.desenhar(screen)
+            chao1.desenhar(screen)
+
+            cacto1.colisao(dino1)
+
+            pygame.display.flip()
+            screen.fill((0, 0, 0))
+    except:
+         pass
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 400))
+clock = pygame.time.Clock()
+pygame.display.set_caption("Dino")
+
+main()
+
+pygame.quit()
